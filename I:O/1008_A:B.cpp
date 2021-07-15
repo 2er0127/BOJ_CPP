a0a24d6c2e7f38c30870861e9561473d980f90c6
princesssuna <1009suna@gmail.com>
2021-07-15 22:03:39 +0900

    7/15 cpp I/O

---
+++ I:O/1008_A:B.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    double A, B;
+    
+    cin>>A;
+    cin>>B;
+    
+    cout.precision(15); //소수점 고정 15
+    cout<<A/B<<endl;
+    
+}
