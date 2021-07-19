9e99f740a000fc0ff7d236668fcf5d9079b4e0b1
princesssuna <1009suna@gmail.com>
2021-07-19 22:52:39 +0900

    7/19 cpp FOR

---
+++ FOR/11021_A+B-7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int n;
+    int a, b;
+    
+    scanf("%d\n", &n);
+    
+    for(int i=1; i<=n; i++){
+        scanf("%d %d\n", &a, &b);
+        printf("Case #%d: %d\n", i, a+b);
+    }
+}
