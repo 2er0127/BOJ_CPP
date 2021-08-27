80d46bbea9037f1bc07d92df185cd2f73a6349f6
princesssuna <1009suna@gmail.com>
2021-08-27 23:43:25 +0900

    forward_list와 벡터 반복자와의 다른 점

--- CPPSTUDY/replicator.cpp
+++ CPPSTUDY/replicator.cpp
@@ -1,3 +1,5 @@
+// 벡터 반복자를 이용해 데이터로부터 유용한 정보를 검색하는 방법
+
 #include <iostream>
 #include <forward_list>
 #include <vector>
@@ -17,5 +19,13 @@ int main() {
     
     advance(it, -3);
     cout<<"그후 3년 뒤 우승자 : "<<*it<<endl;
+    
+    forward_list<string> fwd(vec.begin(), vec.end());
+    
+    auto it1=fwd.begin();
+    cout<<"가장 최근 우승자 : "<<*it1<<endl;
+    
+    advance(it1, 5);
+    cout<<"5년 전 우승자 : "<<*it1<<endl;
     
 }
