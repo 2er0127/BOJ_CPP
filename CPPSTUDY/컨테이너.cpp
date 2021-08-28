91fad5c20c68ef7d7f551b75f8d34b06ea7e0b13
princesssuna <1009suna@gmail.com>
2021-08-28 23:14:13 +0900

    사용자 정의 컨테이너 구현중

---
+++ CPPSTUDY/컨테이너.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct singly_ll_node {
+    int data;
+    singly_ll_node* next;
+};
+
+class singly_ll {
+public:
+    using node=singly_ll_node;
+    using node_ptr=node*;
+    
+private:
+    node_ptr head;
+};
+
+int main() {
+    
+}
