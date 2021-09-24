67e6d3d6e2611382a3cd98d233eb0420eec82654
princesssuna <1009suna@gmail.com>
2021-09-24 20:23:03 +0900

    그래프를 구성하고 인접 행렬로 표현 2/3

--- CPPSTUDY/G_Adjacent matrix.cpp
+++ CPPSTUDY/G_Adjacent matrix.cpp
@@ -8,5 +8,33 @@ enum class city:int {
 };
 
 ostream& operator<<ostream& os, const city c {
-    
-}ß
+    switch(c) {
+        case city::LONDON:os<<"런던";
+            return os;
+        case city::MOSCOW:os<<"모스크바";
+            return os;
+        case city::SEOUL:os<<"서울";
+            return os;
+        case city::SEATTLE:os<<"시애틀";
+            return os;
+        case city::DUBAI:os<<"두바이";
+            return os;
+        case city::SYDNEY:os<<"시드니";
+            return os;
+        default:
+            return os;
+    }
+}
+
+struct graph {
+    vector<vector<int>> data;
+    graph(int n) {
+        data.reserve(n);
+        vector<int> row(n);
+        fill(row.begin(), row.end(), -1);
+        
+        for(int i=0 i<n; i++) {
+            data.push_back(row);
+        }
+    }
+}
