f1bf72a409b58c720fb0d2c26d7af88e1c3f7ee1
princesssuna <1009suna@gmail.com>
2021-10-24 23:08:53 +0900

    학생 예방접종 클래스 구현 2/2

--- Algorithm/Divide and conquer/vaccination.cpp
+++ Algorithm/Divide and conquer/vaccination.cpp
@@ -15,5 +15,20 @@ public:
     //생성자
     Student(pair<int, int> n, bool v) : name(n), vaccinated(v) {}
     
+    //정보 검색
+    auto get_name() {return name;}
+    auto is_vaccinated() {return vaccinated;}
     
-}
+    //이름이 같으면 같은 사람으로 취급
+    bool operator==(const Student& p) const {
+        return this->name==p.name;
+    }
+    
+    //이름을 이용하여 정렬하도록 설정
+    bool operator< (const Student& p) const {
+        return this->name < p.name;
+    }
+    bool operator> (const Student& p) const {
+        return this->name > p.name;
+    }
+};
