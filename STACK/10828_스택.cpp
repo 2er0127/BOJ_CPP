b64813d0aa5a14cd98f260a49e74ea0f7c6ce49a
princesssuna <1009suna@gmail.com>
2022-01-27 23:27:17 +0900

    1/27 스택

--- STACK/10828_스택.cpp
+++ STACK/10828_스택.cpp
@@ -1,74 +1,45 @@
 #include <iostream>
-#include <cstring>
+#include <stack>
+#include <string>
 
 using namespace std;
- 
-//push(data):스택에 데이터를 넣음. 스택의 맨 위(top)에 차지
-//top():스택의 맨 위에 위치한 데이터에 접근. 스택이 비어있는 경우 런타임 에러
-//pop():스택의 맨 위에 위치한 데이터를 빼냄. 스택이 비어있는 경우 런타임 에러
-//size():스택에 현재 저장되어 있는 데이터의 갯수를 반환
-//empty():스택이 비어있는지 확인. 비어있다면 true, 데이터가 남아있으면 false를 반환.
-struct stack{
-    int arr[10000];
-    int ttop; //top에 해당하는 위치를 가리킴.
-    
-    void init(){
-        ttop=-1;
-    }
-    
-    void push(int data){
-        arr[++ttop]=data;
-    }
-    
-    bool empty(){
-        return (ttop<0);
-    }
-    
-    int pop(){
-        if(empty()){
-            return -1; //비어있다면 -1을 출력.
-        }
-        return arr[ttop--];
-    }
-    
-    int size(){
-        return ttop+1;
-    }
-    
-    int top(){
-        if(empty()){
-            return -1; //비어있다면 -1을 출력.
-        }
-        return arr[ttop];
-    }
-};
 
-int main(void){
-    int n;
+int main() {
+    int num, n;
+    string str;
+    stack<int> s;
     
-    cin>>n;
+    cin >> num;
     
-    stack s;
-    s.init();
-    for(; n--; ){
-        string str;
-        int num;
-        cin>>str;
-        if(str=="push"){
-            cin>>num;
-            s.push(num);
+    for(int i = 0; i < num; i++) {
+        cin >> str;
+        if(str == "push") {
+            cin >> n;
+            s.push(n);
         }
-        if(str=="pop"){
-            cout<<s.pop()<<endl;
+        else if(str == "pop") {
+            if(s.empty())
+                cout << -1 <<"\n";
+            else {
+                cout << s.top() << "\n";
+                s.pop();
+            }
         }
-        if(str=="size"){
-            cout<<s.size()<<endl;
+        else if(str == "size") {
+            cout << s.size() << "\n";
         }
-        if(str=="empty"){
-            cout<<s.empty()<<endl;
+        else if(str == "empty") {
+            if(s.empty())
+                cout << 1 << "\n";
+            else
+                cout << 0 <<"\n";
         }
-        if(str=="top"){
-            cout<<s.top()<<endl;
+        else if(str == "top") {
+            if(s.empty())
+                cout << -1 << "\n";
+            else
+                cout << s.top() << "\n";
         }
     }
 }
+
